3ba9fb3fb2cd19c5a34c45153f29ad330015d507
bryanlampert <bryanlampert@gmail.com>
2018-01-16 18:52:22 +0000

    Finish stretch exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -1,9 +1,10 @@
 class Store < ActiveRecord::Base
-  has_many :employees
-
+  has_many :employees, :dependent => :restrict_with_error
   validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, numericality: { greater_than: 0 }
   validate :must_carry_mens_or_womens_apparel
+  before_destroy :check_for_employees
+
 
   def must_carry_mens_or_womens_apparel
     if !mens_apparel && !womens_apparel
@@ -11,4 +12,11 @@ class Store < ActiveRecord::Base
       errors.add(:womens_apparel, "must carry either mens or womens apparel")
     end
   end
+
+  private
+    def check_for_employees
+      if employees.size >= 1
+        errors.add_to_base("Cannot destroy a store with employees!")
+      end
+    end
 end
